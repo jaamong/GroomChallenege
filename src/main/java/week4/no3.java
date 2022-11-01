7edcddbf9df37173867220d4de3139a8193442d5
jaamong <amypure225@naver.com>
2022-11-01 20:08:34 +0900

    정해 코드

--- src/main/java/week4/no3.java
+++ src/main/java/week4/no3.java
@@ -3,13 +3,26 @@ package week4;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class no3 {
-    static int[][] table;
+    static final int MOD = 100_000_007;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        table = new int[3][n];
+        int[][] dp = new int[n + 1][5];
+        for (int[] i : dp) {
+            Arrays.fill(i, 1);
+        }
+
+        for (int i = 2; i < n + 1; i++) {
+            dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2] + dp[i - 1][3] + dp[i - 1][4]) % MOD;
+            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2] + dp[i - 1][3]) % MOD;
+            dp[i][2] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][3] + dp[i - 1][4]) % MOD;
+            dp[i][3] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % MOD;
+            dp[i][4] = (dp[i - 1][0] + dp[i - 1][2]) % MOD;
+        }
+        System.out.println((dp[n][0] + dp[n][1] + dp[n][2] + dp[n][3] + dp[n][4]) % MOD);
     }
 }
