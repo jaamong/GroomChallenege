45cf003a972c89f328be620d7e5ee81ede1815a1
jaamong <amypure225@naver.com>
2022-11-15 14:51:50 +0900

    another correct (시간복잡도 훨씬 낮춤)

--- src/main/java/week6/no2.java
+++ src/main/java/week6/no2.java
@@ -5,12 +5,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class no2 {
+
+    static final char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int length = Integer.parseInt(br.readLine());
         String s = br.readLine();
-
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < length; i += 2) {
@@ -18,10 +20,11 @@ public class no2 {
             int cn = s.charAt(i + 1) - '0';
 
             char ca = s.charAt(i);
-            for (int j = 0; j < cn * cn; j++) {
-                if (ca == 'z') ca = 'a';
-                else ca++;
-            }
+            ca = alphabet[(ca - 'a' + cn * cn) % 26];
+//            for (int j = 0; j < cn * cn; j++) {
+//                if (ca == 'z') ca = 'a';
+//                else ca++;
+//            }
             sb.append(ca);
         }
         System.out.println(sb);
